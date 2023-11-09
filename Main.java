287ee63ff4545b7c832d7981250babfecfc609c4
hiroto <h.nishihara.sys23@morijyobi.ac.jp>
2023-11-09 11:05:04 +0900

    aha

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main  (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Height : ");
+        double height = Integer.parseInt(sc.nextLine());
+        System.out.print("Wight : ");
+        double wight = Integer.parseInt(sc.nextLine());
+
+
+        height /= 100;
+        height *= height;
+
+        double bmi = wight / height;
+        System.out.print("BMI : " + String.format("%.2f", bmi));
+    }
+}
