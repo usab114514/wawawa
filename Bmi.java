cc2c39bd40ba76ed092e64022842e88341785b6e
hiroto <h.nishihara.sys23@morijyobi.ac.jp>
2023-12-12 15:30:30 +0900

    main wo Bmini naosita

--- Bmi.java
+++ Bmi.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void Bmi  (String[] args) {
+public class Bmi {
+    public static void main  (String[] args) {
         Scanner sc = new Scanner(System.in);
 
  	for(int i = 0; i < 2 ; i++) {
